a725363adbc249e5866baaf5cedaacfc0bef8452
hata0 <haataa0100@gmail.com>
2025-05-02 23:21:07 +0900

    fix: envを読み込めるように修正

--- apps/server/src/node.ts
+++ apps/server/src/node.ts
@@ -5,7 +5,7 @@ export const createHandler = (app: OpenAPIHono) => {
   serve(
     {
       fetch: app.fetch,
-      port: 3000,
+      port: Number(process.env.PORT ?? 3000),
     },
     (info) => {
       console.log(`Server is running on http://localhost:${info.port}`);
