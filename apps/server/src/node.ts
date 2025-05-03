9c7e6a97d7660ac678f2829129f910b50cc1648e
hata0 <haataa0100@gmail.com>
2025-05-03 11:37:13 +0900

    feat: portが設定されていない場合throwするように修正

--- apps/server/src/node.ts
+++ apps/server/src/node.ts
@@ -2,10 +2,16 @@ import { serve } from "@hono/node-server";
 import type { OpenAPIHono } from "@hono/zod-openapi";
 
 export const createHandler = (app: OpenAPIHono) => {
+  const port = process.env.PORT;
+
+  if (!port) {
+    throw new Error("port is required");
+  }
+
   serve(
     {
       fetch: app.fetch,
-      port: Number(process.env.PORT ?? 3000),
+      port: Number(port),
     },
     (info) => {
       console.log(`Server is running on http://localhost:${info.port}`);
