e32fb0b7e9c0117905010511bd516f57d34af771
hata0 <haataa0100@gmail.com>
2025-05-09 10:32:21 +0900

    feat: 認証済みかどうか分かりやすくUIを改善

--- apps/web/src/features/top/components/tasks/tasks.tsx
+++ apps/web/src/features/top/components/tasks/tasks.tsx
@@ -1,13 +1,5 @@
-import { Suspense } from "react";
-import { ErrorBoundary } from "react-error-boundary";
 import { TasksList } from "./list";
 
 export const Tasks = () => {
-  return (
-    <ErrorBoundary fallback={<div>タスクの取得に失敗しました</div>}>
-      <Suspense fallback={<div>ロード中</div>}>
-        <TasksList />
-      </Suspense>
-    </ErrorBoundary>
-  );
+  return <TasksList />;
 };
--- apps/web/src/features/top/components/top/top.tsx
+++ apps/web/src/features/top/components/top/top.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { fromPromise } from "neverthrow";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
 import { Tasks } from "../tasks";
 import { TasksForm } from "../tasks-form";
 import { Button } from "@/components/shadcn-ui/button";
@@ -10,36 +12,47 @@ export const Top = () => {
   return (
     <div>
       <main className="flex flex-col justify-center gap-y-4">
-        <Button
-          onClick={async () => {
-            const signInRes = await fromPromise(
-              signInWithPopup(auth, new GoogleAuthProvider()),
-              (e) => e,
-            );
-            if (signInRes.isErr()) {
-              console.error(signInRes.error);
-            } else {
-              const refreshToken = signInRes.value.user.refreshToken;
-              const idToken = await signInRes.value.user.getIdToken();
-              await fetch(
-                `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/auth/firebase`,
-                {
-                  cache: "no-store",
-                  method: "POST",
-                  body: JSON.stringify({
-                    accessToken: idToken,
-                    refreshToken,
-                  }),
-                },
-              );
-            }
-          }}
-          className="w-fit"
+        <ErrorBoundary
+          fallback={
+            <div>
+              <div>タスクの取得に失敗しました。ログインしてください。</div>
+              <Button
+                onClick={async () => {
+                  const signInRes = await fromPromise(
+                    signInWithPopup(auth, new GoogleAuthProvider()),
+                    (e) => e,
+                  );
+                  if (signInRes.isErr()) {
+                    console.error(signInRes.error);
+                  } else {
+                    const refreshToken = signInRes.value.user.refreshToken;
+                    const idToken = await signInRes.value.user.getIdToken();
+                    await fetch(
+                      `${process.env.NEXT_PUBLIC_FRONTEND_URL}/api/auth/firebase`,
+                      {
+                        cache: "no-store",
+                        method: "POST",
+                        body: JSON.stringify({
+                          accessToken: idToken,
+                          refreshToken,
+                        }),
+                      },
+                    );
+                    window.location.reload();
+                  }
+                }}
+                className="w-fit"
+              >
+                Googleでログイン
+              </Button>
+            </div>
+          }
         >
-          Googleでログイン
-        </Button>
-        <TasksForm />
-        <Tasks />
+          <Suspense fallback={<div>ロード中</div>}>
+            <TasksForm />
+            <Tasks />
+          </Suspense>
+        </ErrorBoundary>
       </main>
     </div>
   );
